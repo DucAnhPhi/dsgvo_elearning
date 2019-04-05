483332bd3dedb3f248feb67b246d4dc552fb3ddf
DucAnhPhi <ducanhphi1@gmail.com>
2019-04-05 23:08:00 +0200

    feat: add views

---
+++ src/views/gebot1/gebot1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Gebot1View extends Component {
+    render() {
+        return (
+            <div className="gebote">
+                <h1>1. Gebot: Verbot mit Erlaubnisvorbehalt</h1>
+            </div>
+        )
+    }
+}
+
+export default Gebot1View;
---
+++ src/views/gebot6/gebot6.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Gebot6View extends Component {
+    render() {
+        return (
+            <div className="gebote">
+                <h1>6. Gebot: Dokumentation</h1>
+            </div>
+        )
+    }
+}
+
+export default Gebot6View;
