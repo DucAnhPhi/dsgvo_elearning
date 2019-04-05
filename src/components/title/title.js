e726cb15b045af894ff6940a6605691710f5d678
Sam <samuelmelm@yahoo.de>
2019-04-05 23:35:12 +0200

    feat: styling for title component

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -2,15 +2,10 @@ import './title.css';
 import React, { Component } from 'react';
 
 /*
-  @param class: css class
   @param titleText: text of title
 */
-function Title(props) {
-  if(props.class){
-    return <h1 class={props.class}>{props.string}</h1>;
-  } else{
-    return <h1>{props.titleText}</h1>;
-  }
+function TitleComp(props) {
+    return <h1 class="title">{props.titleText}</h1>;
 }
 
-export default Title;
+export default TitleComp;
